97323d54de195bc02168066e481489a13e7aa12d
amansharma2380 <adipandit2380@gmail.com>
2025-07-10 16:03:44 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './component/Home'
+import Temp from './component/Temp'
+import Scoreboard from './component/Scoreboard'
+import Register from './component/Register'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Cafe-frontend</h1>
+     <Register/>
+     <h3>Footer</h3>
+    </div>
+
+)
+}
+
+export default App
